d0eb5b68034f33f532cd6f4d30046b6a2e7ac672
dafariski77 <rskdafa@gmail.com>
2024-10-05 17:27:20 +0700

    feat: add assets and onboarding screen

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SignIn() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>SignIn</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
